d8d49833d48abc22d4ea141fb6c1a60a52c177a8
Enzo-GH <rm94182@estudante.fieb.edu.br>
2025-06-27 11:20:22 +0000

    Menu

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,10 +7,11 @@ import PokeGrid from "./PokeGrid"
 const Home = ()=> {
     return (
         <div className="Home">
+            <h1>Home</h1>
             <PokeFlex />
             <PokeGrid />
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
